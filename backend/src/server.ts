8d7a48b2b6b888e9a52eb5312e7cfc8cad899a14
MaxLeduc <leduc.maximep@gmail.com>
2020-09-22 23:36:25 -0400

    Adding typescript (#1)
    * adding typescript to both applications
    
    * clean up
    
    * cleanup
    
    * cleanup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 const express = require("express");
+
+const PrimeNumber = require("./prime-number");
+
 const app = express();
 const port = 3000;
 
-app.use("/prime-number", require("./prime-number"));
+app.use("/prime-number", PrimeNumber);
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
